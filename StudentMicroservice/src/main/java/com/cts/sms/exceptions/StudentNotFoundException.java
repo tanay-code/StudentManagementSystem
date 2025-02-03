c61838565b123877a184de7a98b6d86017f82eb3
Tanay Manavat <tanaymanavat2002@gmail.com>
2025-02-03 22:53:55 +0530

    Initial commit of student management system

---
+++ StudentMicroservice/src/main/java/com/cts/sms/exceptions/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cts.sms.exceptions;
+
+public class StudentNotFoundException extends Exception{
+	public StudentNotFoundException(String message) {
+		super(message);
+}
+}
---
+++ service-registry/src/test/java/com/cts/sms/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
